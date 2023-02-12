a738ef89f14cd2265785d9040a14f321ee1b654d
Dorian <dorian.loret20@gmail.com>
2023-02-12 21:28:54 +0100

    end

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-let total = 0;
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
 
-function addition(x) {
-  total += x;
-  return total;
-}
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
 
-function soustraction(x) {
-  total -= x;
-  return total;
-}
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
 
-function division(x) {
-  if (total === 0) {
-    return (total = x);
-  } else {
-    total /= x;
-    return total;
-  }
-}
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
 
-function multiplication(x) {
-  if (total === 0) {
-    return (total = x);
-  } else {
-    total *= x;
-    return total;
-  }
-}
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
 
-function reset() {
-  total = 0;
-}
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter;
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 1000);
